5e7e4b78b79a68e72df5730fc070a27ed248d910
Hank <853160447@qq.com>
2018-06-27 20:24:45 +0800

    str

--- str/str.go
+++ str/str.go
@@ -1,8 +1,27 @@
 package str
 
-import "strconv"
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"crypto/sha256"
+	"strconv"
+)
 
 //Int642str -
 func Int642str(num int64) string {
 	return strconv.FormatInt(num, 10)
+}
+
+//GetHmacSha1 -Sha1加密方式
+func GetHmacSha1(data, key string) string {
+	mac := hmac.New(sha1.New, []byte(key))
+	mac.Write([]byte(data))
+	return string(mac.Sum(nil))
+}
+
+//GetHMacSHA256 -Sha256加密方式
+func GetHMacSHA256(data, key string) string {
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(data))
+	return string(mac.Sum(nil))
 }
