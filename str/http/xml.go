4c355ec2fa9b3f89765134125ef5b525e31dde80
Hank <853160447@qq.com>
2018-06-27 16:08:34 +0800

    xml.go

---
+++ str/http/xml.go
@@ -0,0 +1,27 @@
+package http
+
+import "encoding/xml"
+
+// XMLSerializer -
+type XMLSerializer struct {
+}
+
+// Marshal -
+func (s *XMLSerializer) Marshal(v interface{}) ([]byte, error) {
+	return xml.Marshal(v)
+}
+
+// Unmarshal -
+func (s *XMLSerializer) Unmarshal(data []byte, v interface{}) error {
+	return xml.Unmarshal(data, v)
+}
+
+// GetXML - 基础的网络访问
+func GetXML(url string, values URLEncoder, i interface{}) error {
+	return GetWithSerializer(url, values, i, &XMLSerializer{})
+}
+
+// PostXML - 基础的数据请求
+func PostXML(uri string, values URLEncoder, req interface{}, res interface{}) error {
+	return PostWithSerializer(uri, values, req, res, &XMLSerializer{})
+}
